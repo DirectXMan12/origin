2f378d89f4ad6eac10768e305a38d1fe2d771415
Solly Ross <sross@redhat.com>
2017-06-19 14:08:36 -0400

    Use argparse
    Python has a built-in argument parsing module that provides help text
    and everything.  This will make adding additional arguments easier in
    the future.

--- hack/build-local-images.py
+++ hack/build-local-images.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python2
 
-import sys
 from shutil import copy, rmtree
 import distutils.dir_util as dir_util
 from subprocess import call
@@ -10,9 +9,11 @@ from atexit import register
 from os import getenv, mkdir, remove
 from os.path import abspath, dirname, isdir, join
 
-if len(sys.argv) > 1 and sys.argv[1] in ['-h', '--h', '-help', '--help']:
-    print """Quickly re-build images depending on OpenShift Origin build artifacts.
+import argparse
 
+PARSER = argparse.ArgumentParser(
+    description="Quickly re-build images depending on OpenShift Origin build artifacts.",
+    epilog="""
 This script re-builds OpenShift Origin images quickly. It is intended
 to be used by developers for quick, iterative development of images
 that depend on binaries, RPMs, or other artifacts that the Origin build
@@ -20,20 +21,17 @@ process generates. The script works by creating a temporary context
 directory for a Docker build, adding a simple Dockerfile FROM the image
 you wish to rebuild, ADDing in static files to overwrite, and building.
 
-The script supports ADDing binaries from origin/_output/local/bin/linux/amd64/
-and ADDing static files from the original context directories under the
-origin/images/ directories.
+The script supports ADDing binaries from
+origin/_output/local/bin/linux/amd64/ and ADDing static files from the
+original context directories under the origin/images/ directories.
 
-Usage:
-  [OS_DEBUG=true] [OS_IMAGE_PREFIX=prefix] build-local-images.py [IMAGE...]
+Specific images can be specified to be built with either the full name
+of the image (e.g. openshift3/ose-haproxy-router) or the name sans
+prefix (e.g. haproxy-router).
 
-  Specific images can be specified to be built with either the full name
-  of the image (e.g. openshift3/ose-haproxy-router) or the name sans prefix
-  (e.g. haproxy-router).
-
-  The following environment veriables are honored by this script:
-   - $OS_IMAGE_PREFIX: one of [openshift/origin, openshift3/ose]
-   - $OS_DEBUG: if set, debugging information will be printed
+The following environment veriables are honored by this script:
+- $OS_IMAGE_PREFIX: one of [openshift/origin, openshift3/ose]
+- $OS_DEBUG: if set, debugging information will be printed
 
 Examples:
   # build all images
@@ -44,11 +42,14 @@ Examples:
 
   # build with a different image prefix
   OS_IMAGE_PREFIX=openshift3/ose build-local-images.sh
+""",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+)
 
-Options:
-  -h,--h, -help,--help: show this help-text
-"""
-    exit(2)
+PARSER.add_argument("images", metavar="IMAGE", nargs="*",
+                    help="images to rebuild (e.g. f5-router), or all if no "
+                    "specific images are listed")
+ARGS = PARSER.parse_args()
 
 os_image_prefix = getenv("OS_IMAGE_PREFIX", "openshift/origin")
 image_namespace, image_prefix = os_image_prefix.split("/", 2)
@@ -138,12 +139,14 @@ def image_rebuild_requested(image):
     suffix explicitly or does not provide
     any explicit requests.
     """
-    return len(sys.argv) == 1 or (
-        len(sys.argv) > 1 and (
-            image in sys.argv or
-            full_name(image) in sys.argv
-        )
-    )
+
+    if len(ARGS.images) == 0:
+        return False
+
+    if len(ARGS.images) == 1:
+        return True
+
+    return (image in ARGS.images) or full_name(image) in ARGS.images
 
 
 def full_name(image):
@@ -232,10 +235,10 @@ for image in image_config:
     remove(join(context_dir, "Dockerfile"))
     rmtree(join(context_dir, "src", image))
 
-if not build_occurred and len(sys.argv) > 1:
+if not build_occurred and len(ARGS.images) > 1:
     print ("[ERROR] The provided image names "
            "({}) did not match any buildable images.".format(
-            ", ".join(sys.argv[1:])))
+            ", ".join(ARGS.images)))
     print "[ERROR] This script knows how to build:\n\t{}".format(
         "\n\t".join(map(full_name, image_config.keys()))
     )
